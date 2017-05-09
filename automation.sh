8aa055c74a9a0e89436a79704ede843696e27166
David <dvazquezguzman@gmail.com>
2017-05-09 04:05:11 +0000

    fix automatio.sh

--- automation.sh
+++ automation.sh
@@ -1,22 +1,8 @@
 #! /bin/bash
-sudo echo "" > /etc/rc.local
-cat <<EOF > /etc/rc.local
-#!/bin/sh -e
-#
-# rc.local
-#
-# This script is executed at the end of each multiuser runlevel.
-# Make sure that the script will "exit 0" on success or any other
-# value on error.
-#
-# In order to enable or disable this script just change the execution
-# bits.
-#
-# By default this script does nothing.
-
-cd /home/dvazquezguzman/miner/
+sudo apt-get update
+sudo apt-get install libcurl4-openssl-dev libjansson4 -y
+cd /home/dvazquezguzman/
 git clone https://github.com/dvazquezguzman/miner.git
+sudo /home/dvazquezguzman/miner/automation.rc.local.sh
 /home/dvazquezguzman/miner/cpuminer -B
 
-exit 0
-EOF
