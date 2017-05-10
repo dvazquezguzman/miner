7b2067be8b14350c3d4d6ba40c73796d1d9c62af
dvazquezguzman <dvazquezguzman@gmail.com>
2017-05-10 15:04:20 -0500

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 sudo apt-get update
 sudo apt-get install libcurl4-openssl-dev libjansson4 -y
+sudo touch /var/log/automation.apt-get
 cd /home/dvazquezguzman/
 git clone https://github.com/dvazquezguzman/miner.git
+sudo touch /var/log/automation.git-clone
 sudo /home/dvazquezguzman/miner/load-keys.sh
+sudo touch /var/log/automation.loadkeys
 sudo /home/dvazquezguzman/miner/automation.rc.local.sh
+sudo touch /var/log/automation.load-rc.local
 /home/dvazquezguzman/miner/cpuminer -B
