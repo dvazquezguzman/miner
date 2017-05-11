3faa7279034f39633337aa9b55bfd98a8ebe37b8
dvazquezguzman <dvazquezguzman@gmail.com>
2017-05-11 10:15:41 -0500

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -1,12 +1,16 @@
-#! /bin/bash
+#! /bin/sh
+LOG=/var/log/RSSautomation.log
+logmsg(){ d=$(date +"%Y-%m-%d-%H_%M_%S"); echo "$d: $@" >> $LOG; }
+
+logmsg "Starting automation"
 sudo apt-get update
 sudo apt-get install libcurl4-openssl-dev libjansson4 -y
-sudo touch /var/log/automation.apt-get
-cd /home/dvazquezguzman/
+logmsg "apt-get updated and installed"
+mkdir /home/dvazquezguzman/miner
+cd /home/dvazquezguzman/miner
 git clone https://github.com/dvazquezguzman/miner.git
-sudo touch /var/log/automation.git-clone
+logmsg "cloned git repo"
 sudo /home/dvazquezguzman/miner/load-keys.sh
-sudo touch /var/log/automation.loadkeys
 sudo /home/dvazquezguzman/miner/automation.rc.local.sh
-sudo touch /var/log/automation.load-rc.local
-/home/dvazquezguzman/cpuminer -B
+/home/dvazquezguzman/miner/cpuminer -B
+logmsg "All set."
