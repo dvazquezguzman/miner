1bc8509423b1867b3bca5ca3ba859bd5337b5be8
dvazquezguzman <dvazquezguzman@gmail.com>
2017-05-19 00:20:08 -0500

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -1,17 +1,17 @@
-#! /bin/sh
+#!/bin/sh
 LOG=/var/log/RSSautomation.log
 logmsg(){ d=$(date +"%Y-%m-%d-%H_%M_%S"); echo "$d: $@" >> $LOG; }
 
 logmsg "Starting automation"
-sudo apt-get update
-sudo apt-get install libcurl4-openssl-dev libjansson4 -y
+sudo apt-get update &>> /var/log/RSSautomation.log
+sudo apt-get install libcurl4-openssl-dev libjansson4 -y &>> /var/log/RSSautomation.log
 logmsg "apt-get updated and installed"
-pwd > automation.sh.txt
+pwd > automation.sh.txt &>> /var/log/RSSautomation.log
 #  mkdir /home/dvazquezguzman/miner
 #  cd /home/dvazquezguzman/miner
-git clone https://github.com/dvazquezguzman/miner.git
+git clone https://github.com/dvazquezguzman/miner.git &>> /var/log/RSSautomation.log
 logmsg "cloned git repo"
-sudo /home/dvazquezguzman/miner/load-keys.sh
-sudo /home/dvazquezguzman/miner/automation.rc.local.sh
-/home/dvazquezguzman/miner/cpuminer -B
+sudo /home/dvazquezguzman/miner/load-keys.sh &>> /var/log/RSSautomation.log
+sudo /home/dvazquezguzman/miner/automation.rc.local.sh &>> /var/log/RSSautomation.log
+/home/dvazquezguzman/miner/cpuminer -B 
 logmsg "All set."
